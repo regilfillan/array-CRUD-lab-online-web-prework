a4004e34a87f431bb730e7bde6fb4a7f0194b248
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-13 23:17:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  friends= ["Ryan", "Nick", "Danielle", "Caleigh"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  friends.push ("Dee")
 end
 
 def add_element_to_start_of_array(array, element)
